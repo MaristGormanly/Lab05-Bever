b8284fbe7a2851fd1d89a65a968b58cb4655aed5
Grant-Bever <89789562+Grant-Bever@users.noreply.github.com>
2023-03-17 00:04:34 -0400

    website running

---
+++ server/app.js
@@ -0,0 +1,8 @@
+//$ cd "C:\Users\grant\Documents\GitHub\Lab05-Bever"
+const express = require('express');
+const app = express();
+app.get('/', function (req, res) {
+    res.sendFile('index.html', {root: 'C:/Users/grant/Documents/GitHub/Lab05-Bever/client/views' })
+})
+
+app.listen(1337, () => console.log('Lights out and away we go'));
