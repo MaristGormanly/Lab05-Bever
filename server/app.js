14fd77a5cd6749a7d7dd817ef2dd8445e3143d6d
Grant-Bever <89789562+Grant-Bever@users.noreply.github.com>
2023-03-21 02:52:58 -0400

    changes

--- server/app.js
+++ server/app.js
@@ -4,7 +4,9 @@ const app = express();
 app.get('/', function (req, res) {
     res.sendFile('index.html', {root: 'C:/Users/grant/Documents/GitHub/Lab05-Bever/client/views' })
 })
-
+app.get('/signin', function (req, res) {
+    res.sendFile('page1.html', {root: 'C:/Users/grant/Documents/GitHub/Lab05-Bever/client/views' })
+})
 app.listen(1337, () => console.log('Lights out and away we go'));
 
 app.use(express.static('client/public'));
