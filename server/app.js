0885acc26c1f71dd496b7d140a1c4706f441801d
Grant-Bever <89789562+Grant-Bever@users.noreply.github.com>
2023-03-21 03:18:21 -0400

    added views to server

--- server/app.js
+++ server/app.js
@@ -7,6 +7,12 @@ app.get('/', function (req, res) {
 app.get('/signin', function (req, res) {
     res.sendFile('signin.html', {root: 'C:/Users/grant/Documents/GitHub/Lab05-Bever/client/views' })
 })
+app.get('/signup', function (req, res) {
+    res.sendFile('signup.html', {root: 'C:/Users/grant/Documents/GitHub/Lab05-Bever/client/views' })
+})
+app.get('/home', function (req, res) {
+    res.sendFile('home.html', {root: 'C:/Users/grant/Documents/GitHub/Lab05-Bever/client/views' })
+})
 app.listen(1337, () => console.log('Lights out and away we go'));
 
 app.use(express.static('client/public'));
